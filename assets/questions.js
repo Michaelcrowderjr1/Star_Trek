61955bbbcf6d2b730c8a7d4263218d030743dff9
Michaelcrowderjr1 <70956054+Michaelcrowderjr1@users.noreply.github.com>
2020-10-15 01:59:36 -0400

    Update questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -1,1 +1,39 @@
+//Questions sourced from https://www.funtrivia.com/playquiz/quiz455988e10.html
 
+const questions = [
+    {
+      title: "What is the name of the spaceship aircraft in Star Trek??",
+      choices: ["Air Ship", "Sourcer", "Picard Ship", "Enterprise"],
+      answer: "Enterprise"
+    },
+    {
+      title: "What was the name of the android aboard the ship?",
+      choices: ["Apple", "CPU", "Compute", "Data"],
+      answer: "Data"
+    },
+    {
+      title: "What TV series did William Shatner appear in before Star Trek?",
+      choices: ["The Hitchhiker", "Back to the Future", "Lost in Space", "Twilight Zone"],
+      answer: "Twilight Zone"
+    },
+    {
+      title: "What is the name of the android child that Data builds?",
+      choices: ["Sal", "Pal", "Mandroid", "Lal"],
+      answer: "Lal"
+    },
+    {
+      title: "What are McCoy's medical specialties",
+      choices: ["Ship surgeon", "Surgery and Space psychology", "Medical Doctor", "Gynecologists"],
+      answer: "Surgery and Space psychology"
+    },
+    {
+      title: "What is the proper designation for an Earth-type planet?",
+      choices: ["Class M", "Class B", "Class C", "None of the above"],
+      answer: "Class M"
+    },
+    {
+      title: "What kind of Species is Worf?",
+      choices: ["Human", "Romulan", "Gorn", "Klingon"],
+      answer: "Klingon"
+    }
+]
